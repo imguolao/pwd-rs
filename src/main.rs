a379fa97a7892985d97a9d042eeb23cad10cee61
gromo <gromo.mq@gmail.com>
2022-06-08 22:00:33 +0800

    feat: add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::{env, io};
 
+fn get_current_dir() -> io::Result<String> {
+    let path = format!("{:?}", env::current_dir()?);
+    Ok(path)
+}
+
 fn main() -> io::Result<()> {
-    let path = env::current_dir()?;
-    println!("{}", path.display());
+    let path = match get_current_dir() {
+        Ok(p) => p,
+        Err(e) => panic!("{}", e),
+    };
+
+    println!("{}", path);
+    
     Ok(())
 }
